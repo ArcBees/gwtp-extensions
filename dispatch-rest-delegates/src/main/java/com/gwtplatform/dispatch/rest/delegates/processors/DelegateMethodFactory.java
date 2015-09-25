05e1f4d64b8bc28d4559136a5c66f9cd64242de6
Christopher Viel <viel.christopher@gmail.com>
2015-09-25 11:50:09 -0400

    Generate delegate resource methods

---
+++ dispatch-rest-delegates/src/main/java/com/gwtplatform/dispatch/rest/delegates/processors/DelegateMethodFactory.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.dispatch.rest.delegates.processors;
+
+import javax.lang.model.element.ExecutableElement;
+
+import com.google.auto.service.AutoService;
+import com.gwtplatform.dispatch.rest.processors.endpoint.EndPointMethodFactory;
+import com.gwtplatform.dispatch.rest.processors.resolvers.HttpVerbResolver;
+import com.gwtplatform.dispatch.rest.processors.resource.Resource;
+import com.gwtplatform.dispatch.rest.processors.resource.ResourceMethodFactory;
+import com.gwtplatform.dispatch.rest.processors.resource.ResourceMethodUtils;
+import com.gwtplatform.processors.tools.logger.Logger;
+import com.gwtplatform.processors.tools.utils.Utils;
+
+@AutoService(ResourceMethodFactory.class)
+public class DelegateMethodFactory implements ResourceMethodFactory<DelegateMethod> {
+    private ResourceMethodUtils resourceMethodUtils;
+    private EndPointMethodFactory endPointMethodFactory;
+
+    /**
+     * Explicit constructor required by the Service Loader.
+     */
+    public DelegateMethodFactory() {
+    }
+
+    @Override
+    public void init(Logger logger, Utils utils) {
+        resourceMethodUtils = new ResourceMethodUtils();
+        endPointMethodFactory = new EndPointMethodFactory(logger, utils);
+    }
+
+    @Override
+    public boolean canCreate(ExecutableElement element) {
+        return HttpVerbResolver.isPresent(element) && !resourceMethodUtils.returnsRestAction(element);
+    }
+
+    @Override
+    public DelegateMethod create(Resource parentResource, ExecutableElement element) {
+        return new DelegateMethod(endPointMethodFactory, parentResource, element);
+    }
+}
---
+++ dispatch-rest-delegates/src/main/java/com/gwtplatform/dispatch/rest/delegates/processors/DelegateResourceMethodProcessor.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.dispatch.rest.delegates.processors;
+
+import javax.annotation.processing.ProcessingEnvironment;
+
+import com.google.auto.service.AutoService;
+import com.gwtplatform.dispatch.rest.processors.DispatchRestContextProcessor;
+import com.gwtplatform.dispatch.rest.processors.endpoint.EndPoint;
+import com.gwtplatform.dispatch.rest.processors.endpoint.EndPointProcessor;
+import com.gwtplatform.dispatch.rest.processors.resource.ResourceMethod;
+import com.gwtplatform.dispatch.rest.processors.resource.ResourceMethodProcessor;
+import com.gwtplatform.processors.tools.outputter.CodeSnippet;
+
+import static com.gwtplatform.dispatch.rest.processors.NameUtils.qualifiedMethodName;
+
+@AutoService(ResourceMethodProcessor.class)
+public class DelegateResourceMethodProcessor extends DispatchRestContextProcessor<ResourceMethod, CodeSnippet>
+        implements ResourceMethodProcessor {
+    private static final String TEMPLATE = "/com/gwtplatform/dispatch/rest/delegates/processors/DelegateMethod.vm";
+
+    private final EndPointProcessor endPointProcessor;
+
+    public DelegateResourceMethodProcessor() {
+        endPointProcessor = new EndPointProcessor();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+
+        endPointProcessor.init(processingEnv);
+    }
+
+    @Override
+    public boolean canProcess(ResourceMethod method) {
+        return method instanceof DelegateMethod;
+    }
+
+    @Override
+    public CodeSnippet process(ResourceMethod resourceMethod) {
+        DelegateMethod delegateMethod = (DelegateMethod) resourceMethod;
+        String methodName = qualifiedMethodName(resourceMethod);
+        EndPoint endPoint = delegateMethod.getEndPoint();
+
+        logger.debug("Generating end-point method delegate `%s`.", methodName);
+
+        CodeSnippet code = outputter.withTemplateFile(TEMPLATE)
+                .withParam("method", delegateMethod.getMethod())
+                .withParam("defaultReturnValue", delegateMethod.getDefaultReturnValue())
+                .withParam("endPointType", endPoint.getType())
+                .withParam("endPointArguments", endPoint.getFields())
+                .withErrorLogParameter(methodName)
+                .parse();
+
+        endPointProcessor.process(endPoint);
+
+        return code;
+    }
+
+    @Override
+    public void processLast() {
+        endPointProcessor.processLast();
+    }
+}
